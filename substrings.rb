29d8f53b0dbce1ebb017c4789643a4b293aebb93
JustAida <suradet40@gmail.com>
2022-03-06 08:29:06 +0700

    Fix default value of reduce method

--- substrings.rb
+++ substrings.rb
@@ -1,8 +1,12 @@
 def substrings(word, array)
   # Use reduce method to count.
-  array.reduce(0) do |counter, string|
+  array.reduce(Hash.new(0)) do |counter, string|
     counter[string] += 1 if word.include? string
     counter
   end
 end
 
+dictionary = ["below","down","go","going","horn",
+  "how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("below", dictionary)
