7f8d557d4ffa714e51b8d7bcae6361c7eae8eb34
JustAida <suradet40@gmail.com>
2022-03-06 08:51:31 +0700

    Add a section to handle multiple words

--- substrings.rb
+++ substrings.rb
@@ -1,12 +1,18 @@
-def substrings(word, array)
+def substrings(words, array)
   # Use reduce method to count.
   array.reduce(Hash.new(0)) do |counter, string|
-    counter[string] += 1 if word.include? string
+
+    # Handle multiple words.
+    words.split(" ").each do |word|
+      counter[string] += 1 if word.downcase.include? string.downcase
+    end
+
     counter
   end
 end
 
-dictionary = ["below","down","go","going","horn",
-  "how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", 
+    "it", "i", "low", "own", "part", "partner", "sit"]
 
 puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
